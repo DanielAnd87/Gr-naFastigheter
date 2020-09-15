a99695cdbddab8ae287061d51655ce14b07ffd2f
Daniel Andersson <danzigdanne@gmail.com>
2020-09-15 09:39:19 +0200

    La till Richies kod och navigation till koden.

--- Entities/Models/RealEstate.cs
+++ Entities/Models/RealEstate.cs
@@ -7,18 +7,19 @@ namespace Entities.Models
 {
     public class RealEstate
     {
-        public string CreatedOn { get; set; }
+        public string ImageUrl { get; set; }
+        public string Contact { get; set; }
+        public List<Comment> Comments { get; set; }
+        public DateTime CreatedOn { get; set; }
         public int ConstructionYear { get; set; }
         public string Address { get; set; }
-        public string RealEstateType { get; set; }
+        public object RealEstateType { get; set; }
         public string Description { get; set; }
         public int Id { get; set; }
         public string Title { get; set; }
         public int SellingPrice { get; set; }
-        public int RentingPrice { get; set; }
+        public object RentingPrice { get; set; }
         public bool CanBeSold { get; set; }
         public bool CanBeRented { get; set; }
-        public string ImageUrl { get; set; }
-        
     }
 }
