959977f197678d03df5d3daea650e11b8411c215
Daniel Andersson <danzigdanne@gmail.com>
2020-09-22 16:07:40 +0200

    La till några sökfält.

---
+++ Entities/Models/SearchTerms.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Models
+{
+    public class SearchTerms
+    {
+        public string SearchtermTitle { get; set; } = "";
+        public string SearchtermAdress { get; set; } = "";
+        public string SearchtermType { get; set; } = "";
+        public string OrderBy { get; set; } = "";
+    }
+}
