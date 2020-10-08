4220c661d12e1c4253c7135721e1cfc4dff64628
Linus Örnborg <linus@ornborg.se>
2020-10-08 13:58:31 +0200

    Fixed api cache

--- GrönaFastigheter/wwwroot/serviceworker.js
+++ GrönaFastigheter/wwwroot/serviceworker.js
@@ -1,49 +1,5 @@
 ﻿console.log("This is service worker talking!");
 var CACHE_NAME = 'grona-fast-cache';
-var urlsToCachebck = [
-    './',
-    //Html and css files
-    './index.html',
-    './css/app.dev.css',
-    './css/bootstrap/bootstrap.min.css',
-    './css/open-iconic/font/css/open-iconic-bootstrap.min.css',
-    './css/open-iconic/font/fonts/open-iconic.woff',
-    //Blazor framework
-    './_framework/blazor.webassembly.js',
-    './_framework/blazor.boot.json',
-    //Our additional files
-    './manifest.json',
-    './LinusServiceWorker.js',
-    //'./icons/icon-192x192.png',
-    //'./icons/icon-512x512.png',
-    //The web assembly/.net dll's
-    './_framework/wasm/dotnet.js',
-    './_framework/wasm/dotnet.wasm',
-    './_framework/_bin/WebAssembly.Net.Http.dll',
-    './_framework/_bin/Microsoft.AspNetCore.Blazor.HttpClient.dll',
-    './_framework/_bin/Microsoft.AspNetCore.Blazor.dll',
-    './_framework/_bin/Microsoft.AspNetCore.Components.dll',
-    './_framework/_bin/Microsoft.AspNetCore.Components.Web.dll',
-    './_framework/_bin/Microsoft.Extensions.DependencyInjection.Abstractions.dll',
-    './_framework/_bin/Microsoft.Extensions.DependencyInjection.dll',
-    './_framework/_bin/Microsoft.JSInterop.dll',
-    './_framework/_bin/mscorlib.dll',
-    './_framework/_bin/System.Net.Http.dll',
-    './_framework/_bin/Mono.WebAssembly.Interop.dll',
-    './_framework/_bin/System.dll',
-    './_framework/_bin/System.Core.dll',
-    './_framework/_bin/Microsoft.Bcl.AsyncInterfaces.dll',
-    './_framework/_bin/Microsoft.Extensions.Configuration.Abstractions.dll',
-    './_framework/_bin/Microsoft.Extensions.Logging.Abstractions.dll',
-    './_framework/_bin/Microsoft.Extensions.Primitives.dll',
-    './_framework/_bin/Microsoft.Extensions.Configuration.dll',
-    './_framework/_bin/System.Text.Encodings.Web.dll',
-    './_framework/_bin/System.Text.Json.dll',
-    './_framework/_bin/WebAssembly.Bindings.dll',
-    './_framework/_bin/System.Runtime.CompilerServices.Unsafe.dll',
-    //The compiled project .dll's
-    './_framework/_bin/DotnetPwaSample.dll'
-];
 var urlsToCache = [
     '/',
     'https://blocketliteresourcegroup.azurewebsites.net/api/RealEstates',
@@ -125,14 +81,18 @@ self.addEventListener('activate', function (event) {
 
 self.addEventListener('fetch', function (event) {
     event.respondWith(
-        caches.match(event.request)
-            .then(function (response) {
-                // Cache hit - return response
-                if (response) {
+        (async function () {
+            var cache = await caches.open(CACHE_NAME);
+            var cachedFiles = await cache.match(event.request);
+            if (cachedFiles) {
+                return cachedFiles;
+            } else {
+                try {
+                    var response = await fetch(event.request);
+                    await cache.put(event.request, response.clone());
                     return response;
-                }
-                return fetch(event.request);
+                } catch (e) { /* ... */ }
             }
-            )
-    );
+        }())
+    )
 });
